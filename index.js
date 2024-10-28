f0ffb9e087b7b0163b8ba7dcc4505bfb972ef676
Dawit Misgna <123354278+Dave-misgna@users.noreply.github.com>
2024-10-28 11:59:25 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const button1 = document.getElementById('tom-1');
+const audio1 = document.getElementById('audio_tom-1');
+
+button1.addEventListener('click', ()=>{
+    audio1.play();
+})
+
+const button2 = document.getElementById('tom-2');
+const audio2 = document.getElementById('audio_tom-2');
+button2.addEventListener('click',()=>{
+    audio2.play();
+})
+
+const button3 = document.getElementById('tom-3');
+const audio3 = document.getElementById('audio_tom-3');
+button3.addEventListener('click', ()=>{
+    audio3.play();
+})
+
+const button4 = document.getElementById('tom-4');
+const audio4 = document.getElementById('audio_tom-4');
+button4.addEventListener('click', ()=>{
+    audio4.play();
+})
+
+const button5 = document.getElementById('snare');
+const audio5 = document.getElementById('audio_snare');
+button5.addEventListener('click', ()=>{
+    audio5.play();
+})
+
+const button6 = document.getElementById('crash');
+const audio6 = document.getElementById('audio_crash');
+button6.addEventListener('click', ()=>{
+    audio6.play();
+})
+
+const button7 = document.getElementById('kick-bass');
+const audio7 = document.getElementById('audio_kick-bass');
+
+button7.addEventListener('click', ()=>{
+    audio7.play();
+})
